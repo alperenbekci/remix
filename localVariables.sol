3e8fcbee91806405785b2755b802ee15ae4c514d
alperenbekci <alperenbekci@hotmail.com>
2023-01-22 16:41:26 +0300

    no message

--- localVariables.sol
+++ localVariables.sol
@@ -2,6 +2,14 @@
 
 pragma solidity 0.8.7;
 
+
+
 contract localVariables{
-    
+    //update the state variables
+
+    uint public i = 123;
+    function foo() external{
+        i = 1234;
+        
+    }
 }
